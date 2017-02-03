c7f7d1c8b5b85e102bb5f6d4dd155b17556ca410
tgritter <travis.gritter@gmail.com>
2017-02-03 00:14:22 -0800

    test

--- js/bundle.js
+++ js/bundle.js
@@ -1050,7 +1050,7 @@ window.onload = function () {
         return vars;
     };
 
-    var EXAMPLES = [["DJ", examples.DJ],["Toffoli", examples.TOFFOLI], ["Bell State", examples.BELL_STATE], ["Grover's Algorithm", examples.GROVERS_ALGORITHM], ["Quantum Teleportation", examples.TELEPORTATION]];
+    var EXAMPLES = [["DJ", examples.DJ], ["Grover's Algorithm", examples.GROVERS_ALGORITHM], ["Toffoli", examples.TOFFOLI], ["Bell State", examples.BELL_STATE], ["Quantum Teleportation", examples.TELEPORTATION]];
     var examplesEl = document.querySelector('#examples');
     EXAMPLES.forEach(function (example, i) {
         var name = example[0];
